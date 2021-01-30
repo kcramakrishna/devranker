b7306d55197a08da6c08334f5e7a186f5052558d
dharani <dharani.g@rknowsys.com>
2021-01-30 15:10:43 +0530

    Fix to issues 1, 14 and 38.

--- jupyter_notebooks/devranker_getData_GUI.py
+++ jupyter_notebooks/devranker_getData_GUI.py
@@ -8,8 +8,11 @@ import pathlib
 import pandas
 import json
 import logging
+import more_itertools
 
-####################################################
+# TODO: RAVI-> Check whether it needed or not?
+logging.basicConfig(level=logging.DEBUG)
+
 # Widths of Starting, Middle & Last Widgets
 width_1 = 40
 width_2 = 40
@@ -24,7 +27,6 @@ height_3 = 1
 v_height_1 = 10
 v_height_2 = 10
 v_height_3 = 10
-####################################################
 
 # Flag to find buggy commits.
 # Finding buggy commits entails using 'git blame' extensively. This takes a LOT of time.
@@ -42,14 +44,9 @@ DevrankerDir = ''
 gitDirectory = ''
 DestDirectory = ''
 
-# Variables for 'PySimpleGui'
-dateStart = ''
-dateEnd = ''
+total_commits_count = 0
 
-
-# Sub Layouts(Step1, Step2, Step3 & Step4) Preparation for Main Layout
-
-
+# Sub Layouts(Step1, Step2, Step3 & Step4) will be added to 'layout_main'
 layout_step_1 = [
     [sg.Text('Select the Location of Cloned Repo', text_color='black', background_color='white',
              size=(width_1, height_1)),
@@ -80,20 +77,20 @@ layout_step_1 = [
 
 layout_step_2 = [
 
-    # DATA FILE LOCATION AT
+    # Data File Location
     [sg.Text('Data file location at', text_color='black', background_color='white', size=(width_1, height_1)),
      sg.Input(key='_i_DFL', background_color='white',
               disabled=True, text_color='black', size=(width_2, height_2)),
      sg.Button('Inspect', pad=None, font=('MS Sans Serif', 10, 'bold'),
                button_color=('orange', 'white'), key='_b_Inspect_DFL', size=(width_3, height_3))],
 
-    # ENCRYPT BUTTON
+    # 'Encrypt' Button
     [sg.Text('', background_color='white', text_color='black', size=(width_1, height_1)),
      # sg.Text('', background_color='white', text_color='black', size=(width_2-width_3, height_2-height_3)),
      sg.Button('Encrypt', pad=None, font=('MS Sans Serif', 10, 'bold'),
                button_color=('blue', 'white'), key='_b_Encrypt', size=(width_3, height_3))],
 
-    # ANONYMISED FILE
+    # Anonymised File
     [sg.Text('Anonymised File Located at', background_color='white', text_color='black', border_width=2,
              size=(width_1, height_1)),
      sg.Input(key='_i_AFL', text_color='black',
@@ -101,7 +98,7 @@ layout_step_2 = [
      sg.Button('Inspect', pad=None, font=('MS Sans Serif', 10, 'bold'),
                button_color=('orange', 'white'), key='_b_Inspect_AFL', size=(width_3, height_3))],
 
-    # ANONYMISED DICTIONARY
+    # Anonymised Dictionary
     [sg.Text('Anonymisation Dictionary Located at', background_color='white', text_color='black', border_width=2,
              size=(width_1, height_1)),
      sg.Input(key='_i_ADL', text_color='black',
@@ -109,7 +106,7 @@ layout_step_2 = [
      sg.Button('Inspect', pad=None, font=('MS Sans Serif', 10, 'bold'),
                button_color=('orange', 'white'), key='_b_Inspect_ADL', size=(width_3, height_3))],
 
-    # GET PREDICTIONS BUTTON
+    # 'Get Predictions' Button
     [sg.Text('', background_color='white', text_color='black', size=(width_1, height_1)),
      # sg.Text('', background_color='white', text_color='black', size=(width_2-width_3, height_2-height_3)),
      sg.Button('Get Predictions', pad=None, font=('MS Sans Serif', 10, 'bold'),
@@ -124,7 +121,7 @@ layout_step_3 = [
      sg.Button('Inspect', pad=None, font=('MS Sans Serif', 10, 'bold'),
                button_color=('orange', 'white'), key='_b_Inspect_APF', size=(width_3, height_3))],
 
-    # GET PREDICTIONS BUTTON
+    # 'Decrypt' Button
     [sg.Text('', background_color='white', text_color='black', size=(width_1, height_1)),
      # sg.Text('', background_color='white', text_color='black', size=(width_2-width_3, height_2-height_3)),
      sg.Button('Decrypt', pad=None, font=('MS Sans Serif', 10, 'bold'),
@@ -146,8 +143,7 @@ layout_step_4 = [
                button_color=('blue', 'white'), key='_b_Showcharts', size=(width_3, height_3))],
 ]
 
-####################################################
-# MAIN LAYOUT WHICH IS GOING TO BE ADDED TO 'WINDOW'
+# Main layout going to be added to 'window'
 layout_main = [
     # STEP 1
     [sg.Text('STEP 1', text_color='blue', background_color='white')],
@@ -172,26 +168,27 @@ layout_main = [
 
 
 #  METHODS RELATED TO  'PySimpleGui'
+def update_progress_bar(completed_commits):
+    # lock.acquire()
 
-def update_progress_bar(value):
-    # progressBar.update_bar(value)
-    # progressBarText.update(str(value) + " % ")
-    pass
-    # if value == 100:
-    # progressBarText.update(visible = False)
-    # else:
-    # progressBarText.update(visible = True)
+    len_completed_commits = len(completed_commits)
+
+    logging.info('PROCESS / COMMITS COUNTs:: ' + str(len_completed_commits) + ' / ' + str(total_commits_count))
+
+    progressBar.update_bar(len_completed_commits, total_commits_count)
+    progressBarText.update(str(len_completed_commits) + ' / ' + str(total_commits_count))
+
+    if len(completed_commits) == total_commits_count:
+        progressBarText.update(visible=False)
+        progressBar.update_bar(0, 0)
+    else:
+        progressBarText.update(visible=True)
+
+    # lock.release()
 
 
-#  METHODS RELATED TO  DEVRANKER
-
-# TODO: RAVI -> ADD VALIDATION FOR ALL VARIABLES
-# Reading devrankerClientConfig.txt file data
-
-
-def process_commit(commit, doc_list):
+def process_commit(commit, doc_list, completed_commits):
     for mod in commit.modifications:
-
         # Create a field 'file_ext' which is the file 'type'
         # https://www.geeksforgeeks.org/how-to-get-file-extension-in-python/
         # 'pathlib.Path' gives extension 'None' for all '.' files i.e. .bashrc etc.
@@ -230,20 +227,32 @@ def process_commit(commit, doc_list):
         # List appending is threadsafe: https://stackoverflow.com/a/18568017
         doc_list.append(mod_data)
 
+    completed_commits.append(commit.hash)
+    update_progress_bar(completed_commits)
+
 
 def store_commit_data(git_directory, devranker_dir, output_file_name):
     # Creating empty lists for carrying commit data
     doclist = []
+    # To update progress bar, using list here because it's thread-safe
+    completed_commits = []
 
     # Create Multithreading pool to use full CPU
+    # Ref: https://pythonspeed.com/articles/python-multiprocessing/
     pool = mp.Pool(mp.cpu_count())
 
     # If the Repo has just been cloned, the program will traverse the whole Repo
     # kc - progress bar needs to use the commit number from here or from 'process_commit'
     # https://dzone.com/articles/shared-counter-python%E2%80%99s
-    [pool.apply_async(process_commit(commit, doclist)) for commit in
-     RepositoryMining(git_directory).traverse_commits()]
 
+    commits = RepositoryMining(git_directory).traverse_commits()
+
+    global total_commits_count
+    # 'more_itertools' used here to find commits count as 'commits' is Iterable
+    total_commits_count = more_itertools.ilen(commits)
+
+    [pool.apply_async(process_commit(commit, doclist, completed_commits)) for commit in
+     RepositoryMining(git_directory).traverse_commits()]
     # Close Multiprocessing pool
     pool.close()
     pool.join()
@@ -281,103 +290,121 @@ def validate_directories():
             repo = git.Repo(gitDirectory)
             return True
         except:
-            print('exc 599', sys.exc_info())
+            print('exc @ validate_directories', sys.exc_info())
             sg.popup('Invalid Git Directory, Please choose valid Git Directory')
             return False
 
 
-# Main start of the program
-window = sg.Window('Dev Ranker', layout_main, background_color='white', finalize=True)
+def start_gui_Window():
+    global progressBar
+    global progressBarText
+    global inputDestDir
+    global dataFileLocation
 
-# event, values = window.Read()
-progressBar = window['_pb']
-progressBarText = window['_t_ProgressValue']
-DestDirectory = window['_i_DestDirectory']
-dataFileLocation = window['_i_DFL']
+    global gitDirectory
+    global DestDirectory
 
-# HANDLING ALL EVENTS
+    window = sg.Window('Dev Ranker', layout_main, background_color='white', finalize=True)
+    progressBar = window['_pb']
+    progressBarText = window['_t_ProgressValue']
+    inputDestDir = window['_i_DestDirectory']
+    dataFileLocation = window['_i_DFL']
 
-while True:
+    while True:
+        event, values = window.Read()
+        print(event, values)
 
-    event, values = window.Read()
-    print(event, values)
+        # STEP-1 Related
+        if event in (None, 'Exit'):
+            break
 
-    # STEP1 Related
-    if event in (None, 'Exit'):
-        break
+        elif event == '_i_GitDirectory':
+            gitDirectory = values['_i_GitDirectory']
+            logging.info('_i_GitDirectory', gitDirectory)
+            # Verify if this is a gitRepo right here.
+            try:
+                repo = git.Repo(gitDirectory)
+            except:
+                print('exc after GitDirectory Button clicked ', sys.exc_info())
+                sg.popup('Invalid Git Directory, Please choose valid Git Directory')
+                # kc Need to clear gitDirectory
+                continue
 
-    elif event == '_i_GitDirectory':
-        gitDirectory = values['_i_GitDirectory']
-        logging.info('_i_GitDirectory', gitDirectory)
-        # Verify if this is a gitRepo right here.
-        try:
-            repo = git.Repo(gitDirectory)
-        except:
-            print('exc 599', sys.exc_info())
-            sg.popup('Invalid Git Directory, Please choose valid Git Directory')
-            # kc Need to clear gitDirectory
-            continue
+        elif event == '_i_StartMining':
+            if validate_directories():
+                # https://docs.python.org/3/library/os.path.html
+                devranker_dir = os.path.join(DestDirectory, 'Devranker')
+                if not os.path.exists(devranker_dir):
+                    os.mkdir(devranker_dir)
+                    sg.popup('Created working Directory: ', devranker_dir)
 
-    elif event == '_i_StartMining':
-        if validate_directories():
-            # https://docs.python.org/3/library/os.path.html
-            devranker_dir = os.path.join(DestDirectory, 'Devranker')
-            if not os.path.exists(devranker_dir):
-                os.mkdir(devranker_dir)
-                sg.popup('Created working Directory: ', devranker_dir)
+                # Create a filename from repo name. This is just the name. This is still not a file.
+                repo_name = os.path.basename(gitDirectory)
+                temp_file_name = repo_name + '.git.csv'
+                output_file_name = os.path.join(devranker_dir, temp_file_name)
+                # logging.info("OutputFilename", str(output_file_name))
+            else:
+                # Need to clear gitDirectory and DestDir here or at respective places.
+                continue
+            try:
+                store_commit_data(gitDirectory, devranker_dir, output_file_name)
+            except:
+                logging.info("store_commit failed", '0')
+                continue
 
-            # Create a filename from repo name. This is just the name. This is still not a file.
-            repo_name = os.path.basename(gitDirectory)
-            temp_file_name = repo_name + '.git.csv'
-            output_file_name = os.path.join(devranker_dir, temp_file_name)
-            logging.info("OutputFilename", output_file_name)
-        else:
-            # Need to clear gitDirectory and DestDir here or at respective places.
-            continue
-        try:
-            store_commit_data(gitDirectory, devranker_dir, output_file_name)
-        except:
-            logging.info("store_commit failed", '0')
-            continue
+        elif event == '_i_LiveLog':
+            # update_progress_bar(100)
+            sg.popup('Live Log Clicked')
 
-    elif event == '_i_LiveLog':
-        update_progress_bar(100)
-        sg.popup('Live Log Clicked')
+        elif event == '_i_DestDirectory':
+            DestDirectory = values['_i_DestDirectory']
+            # dataFileLocation.update(outputFileLocation)
+            # liveLogs(devranker_dir, values['_i_DestDirectory'])
 
-    elif event == '_i_DestDirectory':
-        DestDirectory = values['_i_DestDirectory']
-        # dataFileLocation.update(outputFileLocation)
-        # liveLogs(devranker_dir, values['_i_DestDirectory'])
+        # STEP2 Related
+        elif event == '_b_Inspect_DFL':
+            sg.popup('Inspect DFL')
+            # logging.debug('clicked _Inspect_DFL ')
 
-    # STEP2 Related
-    elif event == '_b_Inspect_DFL':
-        sg.popup('Inspect DFL')
+        elif event == '_b_Inspect_AFL':
+            sg.popup('Inspect AFL')
 
-    elif event == '_b_Inspect_AFL':
-        sg.popup('Inspect AFL')
+        elif event == '_b_Inspect_ADL':
+            sg.popup('Inspect ADL')
 
-    elif event == '_b_Inspect_ADL':
-        sg.popup('Inspect ADL')
+        elif event == '_b_GetPredictions':
+            sg.popup('Get Prections Clicked')
 
-    elif event == '_b_GetPredictions':
-        sg.popup('Get Prections Clicked')
+        elif event == '_b_Encrypt':
+            sg.popup('Encrypt Clicked')
 
-    elif event == '_b_Encrypt':
-        sg.popup('Encrypt Clicked')
+        # STEP3 Related
+        elif event == '_b_Inspect_APF':
+            sg.popup('Inspect APF')
 
-    # STEP3 Related
-    elif event == '_b_Inspect_APF':
-        sg.popup('Inspect APF')
+        elif event == '_b_Decrypt':
+            sg.popup('Decrypt Clicked')
 
-    elif event == '_b_Decrypt':
-        sg.popup('Decrypt Clicked')
+        # STEP4 Related
+        elif event == '_b_Inspects_DAPF':
+            sg.popup('Inspect DAPF Clicked')
 
-    # STEP4 Related
-    elif event == '_b_Inspects_DAPF':
-        sg.popup('Inspect DAPF Clicked')
+        elif event == '_b_Showcharts':
+            sg.popup('Show Charts Clicked')
 
-    elif event == '_b_Showcharts':
-        sg.popup('Show Charts Clicked')
+    window.close
 
-# WHEN PRESSED CLOSE
-window.close()
+
+if __name__ == '__main__':
+    # We should call this 'mp.set_start_method("spawn")' here only even though 'mp' used
+    # in 'store_commit_data()' only, because when pressing 'Start Mining' button 2nd time
+    # (Mostly after mining is done) 'mp.set_start_method("spawn")' throwing exception
+
+    # Why 'set_start_method("spawn")'?
+    # Because getting Multiple windows unnecessarily and window became unresponsive after Mining is done
+    # Ref: https://pythonspeed.com/articles/python-multiprocessing/
+
+    mp.set_start_method("spawn")
+
+    # Calling window and initializing required widgets
+    start_gui_Window()
